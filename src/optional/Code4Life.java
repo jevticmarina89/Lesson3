ed2d23e5d4a8a43ca99c35d143446e825bdc236d
jevticmarina89 <marina.jevtic89@gmail.com>
2019-04-30 22:24:10 +0200

    if/else with answers as integers

--- src/optional/Code4Life.java
+++ src/optional/Code4Life.java
@@ -5,19 +5,25 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
-	/*
-	 * Ask the user how many hours they spent coding this week.
-	 * 
-	 * 1. If it's 3 or more, tell them they're a Code Ninja.
-	 * 
-	 * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
-	 * 
-	 * 3. If it's more than 5, play the Batman theme song.
-	 */
+	public static void main(String[] args) {
+		
 	
+	String hours = JOptionPane.showInputDialog("How many hours did you spend coding this week?");
+	int h = Integer.parseInt(hours);
+	if(h>=3 && h<=5) {
+		JOptionPane.showMessageDialog(null, "You are a CODE NINjA!");
+	}
+	else if(h<=2) {
+		JOptionPane.showMessageDialog(null, "Stop watching YouTube and write some code instead.");
+	}
+	else if(h>5) {
+		playBatmanTheme();
+	}
 
+	}
 	private static void playBatmanTheme() {
 		try {
 			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/batman.wav"));
