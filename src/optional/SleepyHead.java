6bfe2ba111b34a285041e1ed98abf8897eda162f
jevticmarina89 <marina.jevtic89@gmail.com>
2019-04-30 22:34:18 +0200

    Good example with if/else nesting

--- src/optional/SleepyHead.java
+++ src/optional/SleepyHead.java
@@ -1,22 +1,39 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 public class SleepyHead {
 
 	public static void main(String[] args) {
 		
 		boolean isWeekday, isVacation;
 		
-		/*
-		 * Ask the user for these values using
-		 * JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
-		 */
-	
-
-		/*
-		 * Print “sleep in” if it is a vacation or a weekend. 
-		 * If it’s a weekday, print "get up lazybones!".
-		 * If it is a weekday, and we are on vacation, print “sleep in”.
-		 */
+		
+		int answer = JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		int answer1 = JOptionPane.showConfirmDialog(null, "is it a weekend?", "", JOptionPane.YES_NO_OPTION);
+		int answer2 = JOptionPane.showConfirmDialog(null, "is it a vacation?", "", JOptionPane.YES_NO_OPTION);
+		
+		if(answer==0) {
+			if(answer2==0) {
+			isWeekday = true;
+			isVacation = true;
+			JOptionPane.showMessageDialog(null,"sleep in!");
+		}}
+		if(answer==0) {
+			if(answer1!=0 && answer2!=0) {
+			isWeekday = true;
+			isVacation = false;
+			JOptionPane.showMessageDialog(null,"get up lazybones!");
+		} }
+		if(answer!=0) {
+			if(answer1==0 || answer2==0) {
+			isWeekday = false;
+			isVacation = true;
+			JOptionPane.showMessageDialog(null,"sleep in!");
+		}
+		}
+		
+		
 	}
 }
 
