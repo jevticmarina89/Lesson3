f08f7045b473b8778f180424494f686a052a5ef6
jevticmarina89 <marina.jevtic89@gmail.com>
2019-04-30 22:39:29 +0200

    Condition is to solve the riddle, if u don't program will give u the answer.

--- src/optional/TheRiddler.java
+++ src/optional/TheRiddler.java
@@ -1,21 +1,42 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
-
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
 		
+
+		int score = 0;
+
+		JOptionPane.showMessageDialog(null, "SOLVE A RIDDLE");
+		String answer = JOptionPane.showInputDialog("I have a tongue but cannot talk.\n I have no legs but sometimes walk. What am I?\nAnswer :");
+		
+		if(answer.equals("a shoe")) {
+			JOptionPane.showMessageDialog(null, "CORRECT!");
+		   score++;}
+		else {
+			JOptionPane.showMessageDialog(null, "WRONG! \n The answer is: a shoe");
+		}
+		
+		String answer1 = JOptionPane.showInputDialog("What has a head and a tail, but no body?\nAnswer:");
+		if(answer1.equals("a coin")) {
+			JOptionPane.showMessageDialog(null, "CORRECT!");
+		   score++;}
+		else {
+			JOptionPane.showMessageDialog(null, "WRONG! \n The answer is: a coin");
+		}
+		
+		String answer2 = JOptionPane.showInputDialog("Which creature walks on four legs in the morning, \n two legs in the afternoon, \n and three legs in the evening?\nAnswer:");
+		if(answer2.equals("a man")) {
+			JOptionPane.showMessageDialog(null, "CORRECT!");
+		   score++;}
+		else {
+			JOptionPane.showMessageDialog(null, "WRONG! \n The answer is: a man");
+		}
+		
+		JOptionPane.showInternalMessageDialog(null, "Your score: " +score);
 	}
 }
 
