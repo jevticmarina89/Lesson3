0034ee9a5bcd75c22328a4cb508d0b8540e080ec
jevticmarina89 <marina.jevtic89@gmail.com>
2019-04-30 21:52:40 +0200

    Guessing game that relies on if condition

--- src/exercises/HighLowGame.java
+++ src/exercises/HighLowGame.java
@@ -1,6 +1,34 @@
 package exercises;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
+	public static void main(String[] args) {
 
+		int n = new Random().nextInt(100) + 1;
+		System.out.println(n);
 
+		for (int i = 4; i >= 0; i--) {
+			String number = JOptionPane.showInputDialog("Guess a number");
+			int m = Integer.parseInt(number);
+
+			if (i != 0) {
+				if (m > n)
+					JOptionPane.showMessageDialog(null, m + " is too high," + i + " more tries");
+				else if (m < n)
+					JOptionPane.showMessageDialog(null, m + " is too low," + i + " more tries");
+				else if (m == n) {
+					JOptionPane.showMessageDialog(null, "You win!");
+					System.exit(0);
+				}
+			}
+			else if (i == 0) {
+				JOptionPane.showMessageDialog(null, "You lose!");
+
+			}
+
+		}
+	}
 }
